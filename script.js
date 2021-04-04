a0d8ac5d46960ed70e62d1a5fa238cdc6efbe201
Alex Bilba <alex.shanless@gmail.com>
2021-04-04 10:16:31 -0700

    js

--- script.js
+++ script.js
@@ -13,4 +13,20 @@ function addNewNote(text = "") {
   </div>
   <div class="main ${text ? "" : "hidden"}"></div>
   <textarea class="${text ? "hidden" : ""}"></textarea>`;
+
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  document.body.appendChild(note);
 }
